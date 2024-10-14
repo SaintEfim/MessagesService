f41c594584e9142570a13630ebe0e27d65c72d4a
SaintEfim <saintefim2@gmail.com>
2024-10-14 22:20:10 +0300

    added read json

--- internal/controller/message_controller.go
+++ internal/controller/message_controller.go
@@ -3,8 +3,10 @@ package controller
 import (
 	"bufio"
 	"context"
+	"encoding/json"
 	"net"
 
+	"MessagesService/internal/models/entity"
 	"MessagesService/internal/models/interfaces"
 
 	"go.uber.org/zap"
@@ -21,7 +23,10 @@ func NewMessageController(logger *zap.Logger) interfaces.MessageController {
 }
 
 func (c *MessageController) MessageProcessRequest(ctx context.Context, conn net.Conn) error {
-	var err error
+	var (
+		err error
+		msg entity.TCPRequest
+	)
 	defer func(conn net.Conn) {
 		err = conn.Close()
 	}(conn)
@@ -35,6 +40,17 @@ func (c *MessageController) MessageProcessRequest(ctx context.Context, conn net.
 
 	for scanner.Scan() {
 		clientMessage := scanner.Text()
+
+		err := json.Unmarshal([]byte(clientMessage), &msg)
+		if err != nil {
+			c.logger.Error("Error parsing JSON", zap.Error(err))
+			conn.Write([]byte("Invalid JSON format.\n"))
+			if err != nil {
+				c.logger.Error("Error sending response:", zap.Error(err))
+				return err
+			}
+			continue
+		}
 
 		c.logger.Info("Received from client", zap.String("clientMessage", clientMessage))
 		_, err = conn.Write([]byte("Message received.\n"))
