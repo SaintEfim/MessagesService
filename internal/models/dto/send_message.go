9d1355ee2de687b5174fe89b2b3158c002e0947d
SaintEfim <saintefim2@gmai.com>
2025-03-11 23:26:41 +0300

    fix validation

--- internal/models/dto/send_message.go
+++ internal/models/dto/send_message.go
@@ -6,5 +6,5 @@ type SendMessage struct {
 	ChatId     uuid.UUID `json:"chat_id"`
 	SenderId   uuid.UUID `json:"sender_id" validate:"required"`
 	ReceiverId uuid.UUID `json:"receiver_id" validate:"required"`
-	Text       string    `json:"text" validate:"gt=0,lte=4096"`
+	Text       string    `json:"text" validate:"gt=0,lte=100"`
 }
